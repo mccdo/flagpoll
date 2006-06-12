6c5f3d0143ddeed09251cd2d736cd2aeefb20fb1
dshipton <dshipton@6e133d5a-c44c-11de-afa3-455a29e9879b>
2006-06-12 15:49:38 +0000

    License crap.

--- flagpoll.py
+++ flagpoll.py
@@ -1,3 +1,37 @@
+#
+#    Flag Poll: A tool to extract flags from installed applications
+#    for compiling, settting variables, etc.
+#
+#    Original Authors:
+#       Daniel E. Shipton <dshipton@gmail.com>
+#
+#    Flag Poll is Copyright (C) 2006 by Daniel E. Shipton
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program; if not, write to:
+#    Free SoftwareFoundation, Inc.
+#    51 Franklin Street, Fifth Floor, 
+#    Boston, MA  02110-1301  USA
+#
+#    -----------------------------------------------------------------
+#    File:          $RCSfile: xmlparser.cpp,v $
+#    Date modified: $Date: 2006/06/02 16:29:03 $
+#    Version:       $Revision: 1.30 $
+#    -----------------------------------------------------------------
+
+
+
+
 from optparse import OptionParser
 from string import Template
 import sys
