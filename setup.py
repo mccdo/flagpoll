f7dc6ea62cb13f7d475d042b1ff7954cafecfc0c
dshipton <dshipton@6e133d5a-c44c-11de-afa3-455a29e9879b>
2006-07-10 19:42:56 +0000

    Options that need to get set so I can build rpms

--- setup.py
+++ setup.py
@@ -13,5 +13,5 @@ setup(
       download_url='https://realityforge.vrsource.org/view/FlagPoll/FlagpollDownloads',
       scripts=['flagpoll'],
       long_description = "Flagpoll is a tool for developers to use meta-data files for storing information on what is needed to compile their software. Think of it as the rpm of software development. It enables developers total control over which packages, versions, architectures, etc. that they want to use meta-data from.",
-      data_files=[('share/flagpoll', ['flagpoll.fpc']), ('share/flagpoll', ['fpcmaker.py'])]
+      data_files=[('share/flagpoll', ['flagpoll.fpc'])]
       )
