e0419426d8c53ad4d80c013430d0c51e0963225a
dshipton <dshipton@6e133d5a-c44c-11de-afa3-455a29e9879b>
2006-07-24 20:08:34 +0000

    Distutils now can make an rpm that installs man pages. Prep the 0.2.1 release

--- trunk/setup.py
+++ trunk/setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
       name='flagpoll',
-      version='0.2.0',
+      version='0.2.1',
       description='Flagpoll is a tool for developers to use meta-data files for storing information on what is needed to compile their software.',
       author='Daniel E. Shipton',
       author_email='dshipton@infiscape.com',
@@ -13,5 +13,5 @@ setup(
       download_url='https://realityforge.vrsource.org/view/FlagPoll/FlagpollDownloads',
       scripts=['flagpoll'],
       long_description = "Flagpoll is a tool for developers to use meta-data files for storing information on what is needed to compile their software. Think of it as the rpm of software development. It enables developers total control over which packages, versions, architectures, etc. that they want to use meta-data from.",
-      data_files=[('share/flagpoll', ['flagpoll.fpc'])]#, ('share/man/man1', ['flagpoll.1'])]
+      data_files=[('share/flagpoll', ['flagpoll.fpc']), ('share/man/man1', ['flagpoll.1'])]
       )
