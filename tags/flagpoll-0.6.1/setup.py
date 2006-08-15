b89c085ed09581ec5aeb1e8ac22a5ea6e7c6a6f1
dshipton <dshipton@6e133d5a-c44c-11de-afa3-455a29e9879b>
2006-08-15 14:35:43 +0000

    Tag 0.6.1

---
+++ tags/flagpoll-0.6.1/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+      name='flagpoll',
+      version='0.6.1',
+      description='Flagpoll is a tool for developers to use meta-data files for storing information on what is needed to compile their software.',
+      author='Daniel E. Shipton',
+      author_email='dshipton@infiscape.com',
+      license='GPL',
+      url='https://realityforge.vrsource.org/view/FlagPoll/WebHome',
+      download_url='https://realityforge.vrsource.org/view/FlagPoll/FlagpollDownloads',
+      scripts=['flagpoll'],
+      long_description = "Flagpoll is a tool for developers to use meta-data files for storing information on what is needed to compile their software. Think of it as the rpm of software development. It enables developers total control over which packages, versions, architectures, etc. that they want to use meta-data from.",
+      data_files=[('share/flagpoll', ['flagpoll.fpc']), ('share/man/man1', ['flagpoll.1']), ('share/aclocal', ['flagpoll.m4'])]
+      )
