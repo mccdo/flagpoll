4abcfff73b54a45156d8de13aa77870834b0c699
dshipton <dshipton@6e133d5a-c44c-11de-afa3-455a29e9879b>
2006-11-09 16:04:37 +0000

    use lambda bindings right and update resolve agents before making depends lists.

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 export FLAGPOLL_PATH=./fpcfiles
+export FLAGPOLL=../flagpoll
 
 for x in `find ./testfiles -name *.test`
 do
    source $x
-   res=`flagpoll $ARGS`
+   res=`$FLAGPOLL $ARGS`
    if test "$RESULTS" != "$res"
    then
       echo "$x failed, '$RESULTS' is not '$res'"
+      echo "--------------debug output----------"
+      $FLAGPOLL $ARGS --debug
    else
       echo "$x passed"
    fi
