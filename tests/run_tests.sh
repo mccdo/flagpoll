3cdb79c618d91a53bcade9a362173c8831b1e759
dshipton <dshipton@6e133d5a-c44c-11de-afa3-455a29e9879b>
2006-11-10 16:35:50 +0000

    Better tests

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -6,11 +6,11 @@ for x in `find ./testfiles -name *.test`
 do
    source $x
    res=`$FLAGPOLL $ARGS`
-   if test "$RESULTS" != "$res"
+   if [ "$RESULTS" != "$res" ]
    then
       echo "$x failed, '$RESULTS' is not '$res'"
-      echo "--------------debug output----------"
-      $FLAGPOLL $ARGS --debug
+      #echo "--------------debug output----------"
+      #$FLAGPOLL $ARGS --debug
    else
       echo "$x passed"
    fi
