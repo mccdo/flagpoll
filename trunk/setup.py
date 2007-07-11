bf50508eadc6ab726627d690bc36507862ee8a2d
dshipton <dshipton@6e133d5a-c44c-11de-afa3-455a29e9879b>
2007-07-11 18:36:17 +0000

    windows installer

--- trunk/setup.py
+++ trunk/setup.py
@@ -18,5 +18,5 @@ setup(
       console=['flagpoll'],
       scripts=['flagpoll'],
       long_description = "Flagpoll is a tool for developers to use meta-data files for storing information on what is needed to compile their software. Think of it as the rpm of software development. It enables developers total control over which packages, versions, architectures, etc. that they want to use meta-data from.",
-      data_files=[('share/flagpoll', ['flagpoll.fpc']), ('share/man/man1', ['flagpoll.1']), ('share/aclocal', ['flagpoll.m4'])]
+      data_files=[('share/flagpoll', ['flagpoll.fpc']), ('share/man/man1', ['flagpoll.1']), ('share/aclocal', ['flagpoll.m4']), ('share/html', ['flagpoll-manual.html'])]
       )
