6b707cd336e7da35c33bb6b6117e0e9a9a77ca3d
doug.mccorkle <doug.mccorkle@6e133d5a-c44c-11de-afa3-455a29e9879b>
2010-09-05 16:42:26 +0000

    Update the docs again for the last time hopefully. Now we can finally do a relase.

--- setup.py
+++ setup.py
@@ -8,13 +8,13 @@ except:
 
 setup(
       name='flagpoll',
-      version='0.9.1',
+      version='0.9.4',
       description='Flagpoll is a tool for developers to use meta-data files for storing information on what is needed to compile their software.',
       author='Daniel E. Shipton',
       author_email='dshipton@infiscape.com',
       license='GPL',
-      url='https://realityforge.vrsource.org/view/FlagPoll/WebHome',
-      download_url='https://realityforge.vrsource.org/view/FlagPoll/FlagpollDownloads',
+      url='http://code.google.com/p/flagpoll/',
+      download_url='http://code.google.com/p/flagpoll/downloads/list',
       console=['flagpoll'],
       scripts=['flagpoll'],
       long_description = "Flagpoll is a tool for developers to use meta-data files for storing information on what is needed to compile their software. Think of it as the rpm of software development. It enables developers total control over which packages, versions, architectures, etc. that they want to use meta-data from.",
